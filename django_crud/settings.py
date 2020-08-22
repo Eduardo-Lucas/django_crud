fa64faa2f82a5f7674fa19201f587e8fa286d367
Eduardo-Lucas <eduardolucas40gmail.com>
2020-08-22 16:50:53 -0300

    configurando para o Heroku

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hlsxgc5!x=4t1**mhy47e1w-km5wv@3hdjy71g^cyw6mc@-h7&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-geekhunter-crud.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
